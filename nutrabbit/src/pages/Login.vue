8748c5b90ee684ca598de3d9b22a61bd2e969a20
aniruddhadivii <aniruddha@divii.com>
2022-03-02 12:40:18 +0530

    up

--- nutrabbit/src/pages/Login.vue
+++ nutrabbit/src/pages/Login.vue
@@ -10,8 +10,25 @@
               <label for="">ID</label>
               <div class="input-group">
                 <div class="input-inner">
-                  <input class="form-control" type="text" placeholder="Enter ID">
+                  <input class="form-control" type="text" placeholder="Enter ID" />
                 </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="">password</label>
+              <div class="input-group">
+                <div class="input-inner">
+                  <input class="form-control" type="text" placeholder="Enter Password" />
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="check-box-wrap">
+                <label class="custom-check">
+                  save ID
+                  <input type="checkbox" />
+                  <span class="checkmark"></span>
+                </label>
               </div>
             </div>
           </form>
