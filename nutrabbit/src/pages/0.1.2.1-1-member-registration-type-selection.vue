ca7a907dde9b9c316d412edb8c30835c21f5171d
aniruddhadivii <aniruddha@divii.com>
2022-03-03 14:08:41 +0530

    up

--- nutrabbit/src/pages/0.1.2.1-1-member-registration-type-selection.vue
+++ nutrabbit/src/pages/0.1.2.1-1-member-registration-type-selection.vue
@@ -4,7 +4,9 @@
     <div class="container-small">
       <div class="login-wrap membership-wrap">
         <div class="login-inner">
+          <div class="login-heading-wrap">
           <h1 class="login-heading">Membership type</h1>
+          </div>
           <p class="membership-desc">Please select a member type</p>
           <div class="membership-list-wrap">
             <ul>
@@ -24,14 +26,17 @@
       </div>
     </div>
   </div>
+  <Footer />
 </template>
 
 <script>
 import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 export default {
   name: "Membership",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
