9e45f9d62bda9ff37ee81826caef817245137376
aniruddhadivii <aniruddha@divii.com>
2022-03-04 19:09:28 +0530

    up

--- nutrabbit/src/pages/member-registration-type-selection.vue
+++ nutrabbit/src/pages/member-registration-type-selection.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="main-body">
-    <div class="container-small">
+    <div class="signUp-container">
       <div class="login-signup-wrap membership-wrap">
         <div class="login-signup-inner">
           <div class="login-heading-wrap">
-          <h1 class="login-heading">Membership type</h1>
-          <p class="membership-desc">Please select a member type</p>
+            <h1 class="login-heading">Personal information management</h1>
+            <p class="membership-desc">Please select a member type</p>
           </div>
-          
+
           <div class="membership-list-wrap">
             <ul>
-              <li class="membership-item active">
+              <li v-for="item in items" :key="item.message">
+                {{ parentMessage }} - {{ index }} - {{ item.message }}
+              </li>
+              <!-- <li class="membership-item active">
                 <i class="icon-user"></i>
                 <h2 class="list-heading">individual membership</h2>
                 <p class="list-desc">When an individual joins</p>
-              </li>
-              <li class="membership-item">
+              </li> -->
+              <!-- <li class="membership-item">
                 <i class="icon-building-symbol"></i>
                 <h2 class="list-heading">Business membership registration</h2>
                 <p When class="list-desc">When a company joins</p>
-              </li>
+              </li> -->
             </ul>
           </div>
         </div>
@@ -29,9 +32,14 @@
 </template>
 
 <script>
-
 export default {
   name: "member-registration-type-selection",
+  data() {
+    return {
+      parentMessage: "Parent",
+      items: [{ message: "Foo" }, { message: "Bar" }],
+    };
+  },
 };
 </script>
 
