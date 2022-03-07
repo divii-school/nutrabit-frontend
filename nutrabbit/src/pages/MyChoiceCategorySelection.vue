0de70495415871172a6da980be4d04cd78de51e8
aniruddhadivii <aniruddha@divii.com>
2022-03-07 17:17:47 +0530

    up

--- nutrabbit/src/pages/MyChoiceCategorySelection.vue
+++ nutrabbit/src/pages/MyChoiceCategorySelection.vue
@@ -1,45 +1,88 @@
 <template>
   <div class="main-body">
     <div class="container-medium">
-      <div class="my-choce-wrap">
+      <div class="my-choce-wrap my-choice-selection">
         <div class="my-choice-heading">
           <h2>my choice</h2>
-          <div class="tolltip-outer">
-            <Popper>
-            <button><i class="icon-info"></i></button>
-            <template #content>
-              <div class="heading-tooltip-content">
-                <ul>
-                  <li>This is a menu where I make my own health functional food recipes.</li>
-                  <li>Please select the desired function.</li>
-                </ul>
-              </div>
-            </template>
-          </Popper>
-          </div>
         </div>
-        <div class="my-choice-list-wrap">
-          <ul class="my-choice-list-outer">
-            <li
-              class="my-choice-list-outer-item"
-              v-for="item of myChoice"
-              :key="item"
-            >
-              <h2 class="list-heading">{{ item.title }}</h2>
-              <ul class="my-choice-list-inner">
-                <li
-                  class="my-choice-list-inner-item"
-                  v-for="item in item.myChoiceListData"
-                  :key="item"
-                >
-                  <div class="choice-card">
-                    <img v-bind:src="item.img" alt="" />
+        <div class="choice-selection-item-wrap">
+          <div class="choice-selection-item recomanded-blending">
+            <div class="heading-wrap">
+              <div class="heading heading-blue">
+                <h2>Recommended blending</h2>
+                <div class="tolltip-outer">
+                  <Popper>
+                    <button><i class="icon-info"></i></button>
+                    <template #content>
+                      <div class="heading-tooltip-content">
+                        <ul>
+                          <li>
+                            This is a menu where I make my own health functional
+                            food recipes.
+                          </li>
+                          <li>Please select the desired function.</li>
+                        </ul>
+                      </div>
+                    </template>
+                  </Popper>
+                </div>
+              </div>
+              <div class="heading-left">
+                <a href=""><i class="icon-plus"></i>see more</a>
+              </div>
+            </div>
+            <ul class="recomanded-list">
+              <li v-for="item in blendingData" :key="item">
+                <SearchCard :item="item" type="searchData" />
+              </li>
+            </ul>
+          </div>
+          <div class="choice-selection-item raw-material">
+            <div class="heading-wrap">
+              <div class="heading">
+                <h2>Recommended blending</h2>
+                <div class="tolltip-outer">
+                  <Popper>
+                    <button><i class="icon-info"></i></button>
+                    <template #content>
+                      <div class="heading-tooltip-content">
+                        <ul>
+                          <li>
+                            This is a menu where I make my own health functional
+                            food recipes.
+                          </li>
+                          <li>Please select the desired function.</li>
+                        </ul>
+                      </div>
+                    </template>
+                  </Popper>
+                </div>
+              </div>
+              <div class="heading-left">
+                <a href=""><i class="icon-plus"></i>see more</a>
+              </div>
+            </div>
+            <ul class="raw-material-list">
+              <li v-for="data in rwaMaterialData" :key="data">
+                <div class="list-left">
+                  <div class="img-wrap">
+                    <img :src="data.img" alt="" />
                   </div>
-                  <p class="desc">{{ item.desc }}</p>
-                </li>
-              </ul>
-            </li>
-          </ul>
+                  <div class="material-details">
+                    <h2>{{ data.title }}</h2>
+                    <div class="description">
+                      <p>{{ data.desc1 }}</p>
+                      <p>{{ data.desc2 }}</p>
+                      <p>{{ data.desc3 }}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="list-right">
+                  <button class="btn-small-solid">test</button>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -50,57 +93,65 @@
 
 <script>
 import Popper from "vue3-popper";
+import SearchCard from "../components/SearchCard.vue";
 export default {
   name: "MyChoiceCategorySelection",
   components: {
     Popper,
+    SearchCard,
   },
   data() {
     return {
-      myChoice: [
+      blendingData: [
         {
-          title: "muscular system",
-          myChoiceListData: [
-            {
-              img: "../../../src/assets/images/choice-img1.svg",
-              desc: "gut health",
-            },
-            {
-              img: "../../../src/assets/images/choice-img1.svg",
-              desc: "gut health",
-            },
-            {
-              img: "../../../src/assets/images/choice-img1.svg",
-              desc: "gut health",
-            },
-            {
-              img: "../../../src/assets/images/choice-img1.svg",
-              desc: "gut health",
-            },
-          ],
-        },
-
-        {
-          title: "nervous system",
-          myChoiceListData: [
-            {
-              img: "../../../src/assets/images/choice-img1.svg",
-              desc: "gut health",
-            },
-            {
-              img: "../../../src/assets/images/choice-img1.svg",
-              desc: "gut health",
-            },
-          ],
+          img: "../../../src/assets/images/aloe-img1.png",
+          desc: "Oridinary blend",
         },
         {
-          title: "digestive/metabolic system",
-          myChoiceListData: [
-            {
-              img: "../../../src/assets/images/choice-img1.svg",
-              desc: "gut health",
-            },
-          ],
+          img: "../../../src/assets/images/aloe-img1.png",
+          desc: "Oridinary blend",
+        },
+        {
+          img: "../../../src/assets/images/aloe-img1.png",
+          desc: "Oridinary blend",
+        },
+        {
+          img: "../../../src/assets/images/aloe-img1.png",
+          desc: "Oridinary blend",
+        },
+      ],
+      rwaMaterialData: [
+        {
+          img: "../../../src/assets/images/raw-material-img.png",
+          title: "Glucomannan (Konjac, Konjac Mannan)",
+          desc1:
+            "improving blood cholesterol Suppression of postprandial blood sugar rise",
+          desc2: "Proliferation of beneficial intestinal bacteria",
+          desc3: "May help smooth bowel movements",
+        },
+        {
+          img: "../../../src/assets/images/raw-material-img.png",
+          title: "Glucomannan (Konjac, Konjac Mannan)",
+          desc1:
+            "improving blood cholesterol Suppression of postprandial blood sugar rise",
+          desc2: "Proliferation of beneficial intestinal bacteria",
+          desc3: "May help smooth bowel movements",
+        },
+        {
+          img: "../../../src/assets/images/raw-material-img.png",
+          title: "Glucomannan (Konjac, Konjac Mannan)",
+          desc1:
+            "improving blood cholesterol Suppression of postprandial blood sugar rise",
+          desc2: "Proliferation of beneficial intestinal bacteria",
+          desc3: "May help smooth bowel movements",
+        },
+        {
+          img: "../../../src/assets/images/raw-material-img.png",
+          title: "Glucomannan (Konjac, Konjac Mannan)",
+          desc1:
+            "improving blood cholesterol Suppression of postprandial blood sugar rise",
+          desc2: "Proliferation of beneficial intestinal bacteria",
+          desc3: "May help smooth bowel movements",
         },
       ],
     };
