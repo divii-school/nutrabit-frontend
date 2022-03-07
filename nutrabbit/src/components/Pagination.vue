533b2064266b09ba0eae5daf054ececcfdb239dd
aniruddhadivii <aniruddha@divii.com>
2022-03-07 09:32:40 +0530

    up

--- nutrabbit/src/components/Pagination.vue
+++ nutrabbit/src/components/Pagination.vue
@@ -6,11 +6,33 @@
       <li>2</li>
       <li>3</li>
     </ul>
-     <a href=""><i class="pagination-arrow-right"></i></a>
+    <a href=""><i class="pagination-arrow-right"></i></a>
   </div>
 </template>
 <script>
 export default {
   name: "Pagination",
+  data() {
+    return {
+      currentPage: 1,
+      perPage: 4,
+      totalData: 0,
+      data: []
+    };
+  },
+  methods:{
+    getData(page = 1){
+      // API call
+      // Update data array
+      // Update totalData no
+      // Update currentPage
+    },
+    onPageClick(page){
+      this.getData(page)
+    }
+  },
+  mounted(){
+    this.getData();
+  },
 };
 </script>
