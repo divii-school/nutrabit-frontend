e9d630689c39e3a8d8620e24e8d000980ec32e88
abhipsaDivii <99310737+abhipsaDivii@users.noreply.github.com>
2022-03-16 17:46:06 +0530

    update

--- nutrabbit/src/pages/Notice.vue
+++ nutrabbit/src/pages/Notice.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-body">
     <div class="container-medium">
-      <div class="my-notic-wrap">
+      <div class="my-notice-wrap">
         <div class="my-notice-heading">
           <h2>Notice</h2>
         </div>
@@ -16,18 +16,135 @@
                 <span>important</span>
                 <p>This is the announcement title. This is the announcement title.</p>
               </div>
-              <div class="item-left">
+              <div class="item-right">
                 <p>2022.01.10</p>
               </div>
             </li>
-          </ul>
+            </ul>
         </div>
+          <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <span>important</span>
+                <p>This is the announcement title. This is the announcement title.</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+          <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <span>important</span>
+                <p>This is the announcement title. This is the announcement title.</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+          <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <span>important</span>
+                <p>This is the announcement title. This is the announcement title.</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+          <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <span>important</span>
+                <p>This is the announcement title. This is the announcement title.</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+         <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <p>This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+         <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <p>This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+         <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <p>This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+         <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <p>This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+         <div class="notice-list">
+          <ul>
+            <li>
+              <div class="item-left">
+                <p>This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title</p>
+              </div>
+              <div class="item-right">
+                <p>2022.01.10</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+         <Pagination />
       </div>
     </div>
   </div>
 </template>
 <script>
+import Pagination from "../components/Pagination.vue";
 export default {
   name: "Notice",
+  components: {
+    Pagination,
+  },
 };
 </script>
