2a777e48f67c19ba88524131cf167948d15dd46f
abhipsaDivii <99310737+abhipsaDivii@users.noreply.github.com>
2022-03-17 19:46:20 +0530

    update

--- nutrabbit/src/pages/CustomerCenter/FAQ.vue
+++ nutrabbit/src/pages/CustomerCenter/FAQ.vue
@@ -6,10 +6,23 @@
           <h2>FAQ</h2>
         </div>
         <div class="faq-heading">
-            <p class="category">category</p>
-            <p class="title">title</p>
+          <p class="category">category</p>
+          <p class="title">title</p>
         </div>
-        <FaqAccordion/>
+        <Faq1Accordion />
+        <div class="details">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque ornare facilisi tempor rutrum blandit duis mauris
+            egestas iaculis. Gravida eu nulla imperdiet turpis massa aliquam,
+            vestibulum gravida tempor. Non fermentum malesuada sit amet
+            vestibulum semper sem mollis. Magna pellentesque bibendum tellus est
+            tortor, faucibus nibh sagittis. Nunc, scelerisque cras convallis
+            duis orci ultrices nec amet eget. Gravida vestibulum, suspendisse eu
+            ut. At at cursus faucibus ac risus mollis.
+          </p>
+        </div>
+         <Pagination />
         <!-- <div class="notice-list">
           <ul>
             <li>
@@ -26,17 +39,18 @@
             </li>
           </ul>
         </div> -->
-        
       </div>
     </div>
   </div>
 </template>
 <script>
-import FaqAccordion from '../../components/FaqAccordion.vue'
+import Faq1Accordion from "../../components/Faq1Accordion.vue";
+import Pagination from "../../components/Pagination.vue";
 export default {
   name: "FAQ",
   components: {
-      FaqAccordion,
+    Faq1Accordion,
+    Pagination,
   },
 };
 </script>
