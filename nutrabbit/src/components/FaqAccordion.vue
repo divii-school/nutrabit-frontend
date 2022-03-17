d9daae71b4843a3bc1cd3571b0105f2b956fb369
abhipsaDivii <99310737+abhipsaDivii@users.noreply.github.com>
2022-03-17 17:12:04 +0530

    update

---
+++ nutrabbit/src/components/FaqAccordion.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <div>Title</div>
+    <div>
+       <slot></slot> 
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "FaqAccordion",
+};
+</script>
--- nutrabbit/src/pages/CustomerCenter/FAQ.vue
+++ nutrabbit/src/pages/CustomerCenter/FAQ.vue
@@ -8,9 +8,9 @@
         <div class="faq-heading">
             <p class="category">category</p>
             <p class="title">title</p>
-          <!-- <p class="date">Published date</p> -->
         </div>
-        <div class="notice-list">
+        <FaqAccordion/>
+        <!-- <div class="notice-list">
           <ul>
             <li>
               <div class="item-left">
@@ -25,156 +25,18 @@
               </div>
             </li>
           </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">estimate</p>
-                <span>important</span>
-                <p>
-                  This is the FAQ title.
-                </p>
-              </div>
-              <div class="item-right">
-               <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">Sample</p>
-                <span>important</span>
-                <p>
-                  This is the FAQ title..
-                </p>
-              </div>
-              <div class="item-right">
-                <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">delivery</p>
-                <span>important</span>
-                <p>
-                 This is the FAQ title.
-                </p>
-              </div>
-              <div class="item-right">
-                <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">payment</p>
-                <span>important</span>
-                <p>
-                 This is the FAQ title.
-                </p>
-              </div>
-              <div class="item-right">
-                <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">service</p>
-                   <span>important</span>
-                <p>
-                  This is the FAQ title.
-                </p>
-              </div>
-              <div class="item-right">
-                 <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">Etc</p>
-                   <span>important</span>
-                <p>
-                  This is the FAQ title.
-                </p>
-              </div>
-              <div class="item-right">
-                 <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">service</p>
-                <p>
-                 This is the FAQ title.
-                </p>
-              </div>
-              <div class="item-right">
-                 <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">service</p>
-                <p>
-                 This is the FAQ title.
-                </p>
-              </div>
-              <div class="item-right">
-                <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="notice-list">
-          <ul>
-            <li>
-              <div class="item-left">
-                   <p class="para-category">service</p>
-                <p>
-                  This is the FAQ title.
-                </p>
-              </div>
-              <div class="item-right">
-            <!-- <vue-select  class="inquiry-select" :options="['', '']"  close-on-select> </vue-select> -->
-            
-             <img src="src/assets/icons/icon-arrowDown.svg" />
-              </div>
-            </li>
-          </ul>
-        </div>
+        </div> -->
+        
       </div>
     </div>
   </div>
 </template>
 <script>
+import FaqAccordion from '../../components/FaqAccordion.vue'
 export default {
   name: "FAQ",
+  components: {
+      FaqAccordion,
+  },
 };
 </script>
