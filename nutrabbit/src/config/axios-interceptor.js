48fddd1bed25a9aa2918c4a566233170f8361510
kakalidivii <102513275+kakalidivii@users.noreply.github.com>
2022-03-29 17:36:26 +0530

    update

--- nutrabbit/src/config/axios-interceptor.js
+++ nutrabbit/src/config/axios-interceptor.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 
 window.axios = axios;
-axios.defaults.baseURL = 'http://api-nutrabbit-dev.dvconsulting.org:3000/nutrabbit-dev/api/v1/sites/';
+axios.defaults.baseURL = 'http://api-nutrabbit-dev.dvconsulting.org:3000/nutrabbit-dev/api';
 
 axios.defaults.headers = {
     source:'nutrabbit',
     apiKey:'coN21di1202VII01Ed0OnNiMDa2P3p0M',
-    token: localStorage.getItem('token'),
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjEsInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjQ4NTUyMjczLCJleHAiOjE2NDg1NTU4NzN9.nSfO9WeAd1TTv0cJxrZHQbumcssfEEdZKpS1NkA15s4',
     'Accept-Language':'en'
 };
--- nutrabbit/src/pages/Main/Main.vue
+++ nutrabbit/src/pages/Main/Main.vue
@@ -10,7 +10,10 @@
         class="mySwiper"
       >
         <swiper-slide v-for="(slider, index) in MainSlider" :key="index">
-          <img :src="slider.img" alt="" />
+          <img
+            :src="'http://api-nutrabbit-dev.dvconsulting.org/public/' + slider.desktop_banner_path"
+            alt
+          />
           <p class="banner-title text-center">{{ slider.title }}</p>
         </swiper-slide>
       </swiper>
@@ -21,11 +24,12 @@
           <span class="my-choice-title-top">my choice</span>
           <p class="title text-center">my choice</p>
           <h2 class="nutri-choice-heading text-center">
-            Direct health functional food recipes<br />Opportunity to create!
+            Direct health functional food recipes
+            <br />Opportunity to create!
           </h2>
-          <p class="desc text-center">
-            Create your own recipe with just one combination of your choice!
-          </p>
+          <p
+            class="desc text-center"
+          >Create your own recipe with just one combination of your choice!</p>
           <button class="btn-small-solid">Go to my choice</button>
         </div>
       </div>
@@ -38,15 +42,14 @@
             <span class="my-choice-title-top">nutri 3.3</span>
             <p class="title text-center">nutri 3.3 blending</p>
             <h2 class="nutri-choice-heading text-center">
-              service only for you<br />Easily seize the opportunity to launch
+              service only for you
+              <br />Easily seize the opportunity to launch
               your own product!
             </h2>
-            <p class="desc text-center">
-              We provide all services from A to Z of health functional food.
-            </p>
-            <button class="btn-small-solid green">
-              What is nutri 3.3 Blending?
-            </button>
+            <p
+              class="desc text-center"
+            >We provide all services from A to Z of health functional food.</p>
+            <button class="btn-small-solid green">What is nutri 3.3 Blending?</button>
           </div>
           <div class="nutri-dom-product">
             <ul>
@@ -76,20 +79,7 @@ export default {
   },
   data() {
     return {
-      MainSlider: [
-        {
-          img: "../../../src/assets/images/main-banner.png",
-          title: "Event Banner",
-        },
-        {
-          img: "../../../src/assets/images/main-banner.png",
-          title: "Event Banner",
-        },
-        {
-          img: "../../../src/assets/images/main-banner.png",
-          title: "Event Banner",
-        },
-      ],
+      MainSlider: [],
       ProductData: [
         {
           img: "../../../src/assets/images/dom-pimg.png",
@@ -160,6 +150,7 @@ export default {
     };
   },
   mounted() {
+    this.created();
     if (localStorage.getItem("logedInUserDetails")) {
       this.logedInUserDetails =
         JSON.parse(localStorage.getItem("logedInUserDetails")) || {};
@@ -167,5 +158,24 @@ export default {
       this.logedInUserDetails = null;
     }
   },
+  methods: {
+    async created() {
+      try {
+        const data = await axios.post("/v1/sites/banner", {
+          lang: "KO"
+        })
+          .then((response) => {
+            if (response.data.status == 200) {
+              this.MainSlider = response.data.data.bannerData;
+              // console.log(response.data.data.bannerData);
+            }
+          });
+      }
+      catch (e) {
+        console.error(e);
+      }
+    },
+  }
+
 };
 </script>
