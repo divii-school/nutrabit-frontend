a74b71b612ce3d00ea2a03b8b521fd6a0756fdb0
Bikramjit Show <bikramjit@divii.com>
2022-04-05 14:37:32 +0530

    add

--- nutrabbit/src/pages/Main/Main.vue
+++ nutrabbit/src/pages/Main/Main.vue
@@ -37,6 +37,7 @@
             <button class="btn-small-solid">Go to my choice</button>
           </router-link>
         </div>
+            {{ 'ENV = ' +  process.env.VITE_SOME_KEY }}
       </div>
       <div class="devider">
         <i class="icon-grey-star"></i>
--- nutrabbit/vite.config.js
+++ nutrabbit/vite.config.js
@@ -1,17 +1,39 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/styles/scss/style.scss";`
+// export default defineConfig({
+//   plugins: [vue()],
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `@import "src/styles/scss/style.scss";`
+//       },
+//     },
+//   },
+//   server: {
+//     port: 3001,
+//   },
+// })
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [vue()],
+
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/scss/style.scss";`
+        },
       },
     },
-  },
-  server: {
-    port: 3001,
-  },
-})
+
+    server: {
+      port: process.env.VITE_PORT,
+    },
+  });
+}
