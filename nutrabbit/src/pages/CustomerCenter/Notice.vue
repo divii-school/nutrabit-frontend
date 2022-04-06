c643b061e162870fcea1a605bf7f2ed5039ae60d
aniruddhadivii <aniruddha@divii.com>
2022-04-06 17:50:26 +0530

    up

--- nutrabbit/src/pages/CustomerCenter/Notice.vue
+++ nutrabbit/src/pages/CustomerCenter/Notice.vue
@@ -11,82 +11,53 @@
         </div>
         <div class="notice-list bBtm-0">
           <ul>
-            <li v-for="(item, index) of NoticeList" :key="index">
-              <div class="item-left">
-                <span v-if="item.tag">{{item.tag}}</span>
-                <p>{{item.title}}</p>
+            <!-- <li v-for="(item, index) of NoticeList" :key="index">
+              <div class="item-left"> -->
+                <!-- <span v-if="item.tag">{{ item.tag }}</span> -->
+                <!-- <p>{{ item.title_ko }}</p>
               </div>
-              <div class="item-right">
-                <p>{{item.date}}</p>
-              </div>
-            </li>
-            </ul>
+              <div class="item-right"> -->
+                <!-- <p>{{ item.date }}</p> -->
+              <!-- </div>
+            </li> -->
+          </ul>
         </div>
-         <Pagination />
+        <Pagination />
       </div>
     </div>
   </div>
 </template>
 <script>
 import Pagination from "../../components/Pagination.vue";
+import CustomerCenterService from "../../services/CustomerCenterService";
 export default {
   name: "Notice",
   components: {
     Pagination,
   },
-   data(){
-    return{
-      NoticeList:[
-        {
-          tag:"important",
-          title:"This is the announcement title. This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          tag:"important",
-          title:"This is the announcement title. This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          tag:"important",
-          title:"This is the announcement title. This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          tag:"important",
-          title:"This is the announcement title. This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          tag:"important",
-          title:"This is the announcement title. This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          title:"This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          title:"This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          title:"This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          title:"This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title.",
-          date:"2022.01.10"
-        },
-        {
-          title:"This is the announcement title. This is the announcement title.This is the announcement title.This is the announcement title.",
-          date:"2022.01.10"
-        }
-      ]
-    }
+  data() {
+    return {
+      NoticeList: [],
+    };
   },
-  mathods:{
-    
-  }
+  created() {
+    this.CustomerCenterService = new CustomerCenterService();
+  },
+  mounted() {
+    this.allNoticeList()
+  },
+  mathods: {
+    allNoticeList() {
+      this.CustomerCenterService.getNoticeList().then((res) => {
+        if (res.status == 200) {
+          console.log(res.data);
+          // this.NoticeList = res.data;
+          // console.log(this.NoticeList);
+        } else {
+          console.log("ghj");
+        }
+      });
+    },
+  },
 };
 </script>
--- nutrabbit/src/services/CustomerCenterService.js
+++ nutrabbit/src/services/CustomerCenterService.js
@@ -3,13 +3,8 @@ import axios from 'axios';
 
 export default class CustomerCenterService {
 
-  // Notice List
-  async getNoticeList() {
-    return await axios.post(`/user/individual_registration`, {
-        lang: 'KO',
-        title: ''
-    })
-      .then((res) => res)
-      .catch((err) => err)
-  }
+    // Notice List
+    async getNoticeList() {
+        return await axios.post(`/notice`, { lang: 'KO', title: '' }).then((res) => res.data).catch((err) => err);
+    }
 }
