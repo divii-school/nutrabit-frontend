0d1418793f5185b0977485e4d6b1e44ea6557332
Phenil810 <100286762+Phenil810@users.noreply.github.com>
2022-04-07 13:13:15 +0530

    updated

--- nutrabit-admin/src/pages/BusinessMember/ViewBusiness.vue
+++ nutrabit-admin/src/pages/BusinessMember/ViewBusiness.vue
@@ -3,110 +3,174 @@
     <div class="p-grid">
         <!-- <div class="p-col-12 p-pb-0">
             <Button @click="$router.go(-1)" :label="$t('button.back')" icon="pi pi-angle-left" class="p-button-text p-mr-2 p-mb-2" />
-        </div> -->
+        </div>-->
         <div class="p-col-12">
             <div class="card p-fluid">
-                <h4><strong>{{$t('Business.list.Businessheader')}}</strong></h4>
+                <h4>
+                    <strong>{{ $t('Business.list.Businessheader') }}</strong>
+                </h4>
 
                 <div class="p-field p-grid">
-					<label for="nameuser" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.BusinessNumber') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.business_number }}</p>
-					</div> 
-				</div>
+                    <label
+                        for="nameuser"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.BusinessNumber') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.business_number }}</p>
+                    </div>
+                </div>
 
                 <div class="p-field p-grid">
-					<label for="business_name" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.BusinessName') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.business_name }}</p>
-					</div> 
-				</div>
-
-                
-                <div class="p-field p-grid">
-					<label for="department" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.Departmentname') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.department }}</p>
-					</div> 
-				</div>
+                    <label
+                        for="business_name"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.BusinessName') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.business_name }}</p>
+                    </div>
+                </div>
 
                 <div class="p-field p-grid">
-					<label for="person_in_charge" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.Personincharge') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.person_in_charge }}</p>
-					</div> 
-				</div>
+                    <label
+                        for="department"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.Departmentname') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.department }}</p>
+                    </div>
+                </div>
 
                 <div class="p-field p-grid">
-					<label for="uuid" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.ID') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.uuid }}</p>
-					</div> 
-				</div>
+                    <label
+                        for="person_in_charge"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.Personincharge') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.person_in_charge }}</p>
+                    </div>
+                </div>
 
                 <div class="p-field p-grid">
-					<label for="password" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.Password') }}:</label>
-					<div class="p-col-12 p-md-10" style="display: flex; justify-content: flex-start">
-						<p >********</p>  
-                        <a href="#" @click="open" class="p-button p-button-outlined p-button-sm p-mr-2 p-mb-2" style=" background: white;border: 1px solid #0A0A0A;color: #000000; margin-left: 1%;">{{$t('Change Password')}}</a>
-					</div>
-				</div>
-               
-               
-                <div class="p-field p-grid">
-					<label for="email" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.Email') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.email }}</p>
-					</div> 
-				</div>
+                    <label
+                        for="uuid"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.ID') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.uuid }}</p>
+                    </div>
+                </div>
 
                 <div class="p-field p-grid">
-					<label for="mobile" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.mobile') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.mobile }}</p>
-					</div> 
-				</div>
+                    <label
+                        for="password"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.Password') }}:</label>
+                    <div
+                        class="p-col-12 p-md-10"
+                        style="display: flex; justify-content: flex-start"
+                    >
+                        <p>********</p>
+                        <a
+                            href="#"
+                            @click="open"
+                            class="p-button p-button-outlined p-button-sm p-mr-2 p-mb-2"
+                            style=" background: white;border: 1px solid #0A0A0A;color: #000000; margin-left: 1%;"
+                        >{{ $t('Change Password') }}</a>
+                    </div>
+                </div>
 
                 <div class="p-field p-grid">
-					<label for="address" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.Area') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.address }}</p>
-					</div> 
-				</div>
+                    <label
+                        for="email"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.Email') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.email }}</p>
+                    </div>
+                </div>
 
                 <div class="p-field p-grid">
-					<label for="createdDate" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">{{ $t('Business.list.RegistrationDate') }}:</label>
-					<div class="p-col-12 p-md-10">
-						<p >{{ mydata.createdDate }}</p>
-					</div> 
-				</div>
-                    
-                    
-                
+                    <label
+                        for="mobile"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.mobile') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.mobile }}</p>
+                    </div>
+                </div>
+
+                <div class="p-field p-grid">
+                    <label
+                        for="address"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.Area') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.address }}</p>
+                    </div>
+                </div>
+
+                <div class="p-field p-grid">
+                    <label
+                        for="createdDate"
+                        class="p-col-12 p-mb-2 p-md-2 p-mb-md-0"
+                    >{{ $t('Business.list.RegistrationDate') }}:</label>
+                    <div class="p-col-12 p-md-10">
+                        <p>{{ mydata.createdDate }}</p>
+                    </div>
+                </div>
 
                 <div class="p-d-flex p-jc-end" style="float:left;">
                     <!-- <router-link to="/view-user"
                         ><Button label="info" class="p-button-outlined p-button-info p-mr-2 p-mb-2" disabled><i class="pi pi-eye p-mr-2"></i>point</Button>
-                    </router-link> -->
-                    <router-link :to="'/business-edit/' + $route.params.id"
-                        ><Button label="help" class="p-button p-button-outlined p-button-sm p-mr-2 p-mb-2"><i class="pi pi-user-edit p-mr-2"></i> {{ $t('button.edit') }}</Button></router-link
-                    >
+                    </router-link>-->
+                    <router-link :to="'/business-edit/' + $route.params.id">
+                        <Button
+                            label="help"
+                            class="p-button p-button-outlined p-button-sm p-mr-2 p-mb-2"
+                        >
+                            <i class="pi pi-user-edit p-mr-2"></i>
+                            {{ $t('button.edit') }}
+                        </Button>
+                    </router-link>
                 </div>
-                     <div class="p-d-flex p-jc-end">
-                    <Button @click="del($route.params.id)" label="delete" class="p-button p-button-outlined p-button-sm p-mr-2 p-mb-2"><i class="pi pi-trash p-mr-2"></i> {{ $t('button.delete') }}</Button>
+                <div class="p-d-flex p-jc-end">
+                    <Button
+                        @click="del($route.params.id)"
+                        label="delete"
+                        class="p-button p-button-outlined p-button-sm p-mr-2 p-mb-2"
+                    >
+                        <i class="pi pi-trash p-mr-2"></i>
+                        {{ $t('button.delete') }}
+                    </Button>
                 </div>
             </div>
         </div>
-         <Dialog :header="$t('Individual.list.changepass')" v-model:visible="display" :breakpoints="{ '960px': '75vw' }" :style="{ width: '30vw' }" :modal="true">
+        <Dialog
+            :header="$t('Individual.list.changepass')"
+            v-model:visible="display"
+            :breakpoints="{ '960px': '75vw' }"
+            :style="{ width: '30vw' }"
+            :modal="true"
+        >
             <div class="p-field p-fluid">
-                <label for="username">{{$t('Individual.list.newpassword')}}:</label>
-                <InputText  id="password" type="password" v-model="new_password" />
-                <small v-show="helptxt">{{$t('password.help')}}</small>
+                <label for="username">{{ $t('Individual.list.newpassword') }}:</label>
+                <InputText id="password" type="password" v-model="new_password" />
+                <small v-show="helptxt">{{ $t('password.help') }}</small>
                 <!-- <div class="text-redx">{{ error.new_password }}{{ errmsg }}</div> -->
             </div>
             <template #footer>
-                <Button :label="$t('Individual.list.confirm')" @click="resetpass" class="p-button" style="background:white;color:black;border:0px solid; margin-right:60px;"/>
-                <Button :label="$t('Individual.list.cancel')" @click="close" class="p-button"  style="float:left; background:white;color:black;border:0px solid;margin-left:60px;"/>
+                <Button
+                    :label="$t('Individual.list.confirm')"
+                    @click="resetpass"
+                    class="p-button"
+                    style="background:white;color:black;border:0px solid; margin-right:60px;"
+                />
+                <Button
+                    :label="$t('Individual.list.cancel')"
+                    @click="close"
+                    class="p-button"
+                    style="float:left; background:white;color:black;border:0px solid;margin-left:60px;"
+                />
             </template>
         </Dialog>
     </div>
@@ -124,25 +188,25 @@ export default {
                 { name: 'others', code: 'others' },
             ],
             dropdownItem: null,
-            new_password:'',
+            new_password: '',
             display: false,
             mydata: {
                 // name: '',
                 // email: '',
-                 status:'',
-            id:'',
-            business_number:'',
-            business_name:'',
-            department:'', 
-            person_in_charge:'',
-            email:'',
-            account_type:'',
-            username:'',
-            uuid:'',
+                status: '',
+                id: '',
+                business_number: '',
+                business_name: '',
+                department: '',
+                person_in_charge: '',
+                email: '',
+                account_type: '',
+                username: '',
+                uuid: '',
                 mobile: '',
                 createdDate: '',
-                address:'',
-                password:'',
+                address: '',
+                password: '',
             },
         };
     },
@@ -202,29 +266,28 @@ export default {
             //     console.log(error);
             // } 
             // else {
-                this.errmsg = '';
-                var self = this;
-                this.helptxt = true;
-                this.error = {};
-                axios({
-                    method: 'post',
-                    url: '/admin/user/change_password',
-                    data: { id:this.$route.params.id,new_password: this.new_password },
-                    // this.$router.push({ name: 'BannerManagement' });
+            this.errmsg = '';
+            var self = this;
+            this.helptxt = true;
+            this.error = {};
+            axios({
+                method: 'post',
+                url: '/admin/user/change_password',
+                data: { id: this.$route.params.id, new_password: this.new_password },
+                // this.$router.push({ name: 'BannerManagement' });
+            })
+                .then(function (res) {
+                    console.log(res.data.status)
+                    if (res.data.status == 200) {
+                        alert('비밀번호가 성공적으로 변경되었습니다.')
+                    }
+                    self.close();
+                    self.$toast.add({ severity: 'success', summary: '비밀번호가 성공적으로 변경되었습니다', detail: ` 새로운 비밀번호가 ${self.new_password}로 발송되었습니다  `, life: 5000 });
                 })
-                   .then(function (res) {
-                       console.log(res.data.status)
-                        if(res.data.status == 200)
-                        {
-                            alert('비밀번호가 성공적으로 변경되었습니다.')
-                        }
-                        self.close();
-                        self.$toast.add({ severity: 'success', summary: '비밀번호가 성공적으로 변경되었습니다', detail: ` 새로운 비밀번호가 ${self.new_password}로 발송되었습니다  `, life: 5000 });
-                    })
-                    .catch((err) => {
-                        this.helptxt = false;
-                        this.errmsg = err.response.data.message;
-                    });
+                .catch((err) => {
+                    this.helptxt = false;
+                    this.errmsg = err.response.data.message;
+                });
             // }
         },
     },
@@ -240,7 +303,7 @@ export default {
             this.mydata.mobile = res.data.data[0].mobile;
             this.mydata.address = res.data.data[0].address;
             this.mydata.createdDate = res.data.data[0].createdDate;
-           
+
         });
     },
 };
@@ -250,9 +313,9 @@ export default {
 .p-fluid .p-button {
     width: auto;
 }
-.p-button{
+.p-button {
     background: #000000;
-    border: 1px solid #0A0A0A;
+    border: 1px solid #0a0a0a;
     color: white;
 }
 </style>
