fac21420a05c0143a5e361d466adacf1abd20c04
jayantavue <jayanta@divii.com>
2022-04-10 07:41:05 +0530

    upadted recipe service

--- nutrabbit/src/services/MyRecipeService.js
+++ nutrabbit/src/services/MyRecipeService.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 var token = localStorage.getItem('token');
 
 export default class MyRecipeService{
+
    async getMyRecomendedBlendingData(uid){
         return await axios.post(`/recipe`, { lang: 'KO',  user_id : uid, application_type : 'recommended_blending'}).then((res) => res.data).catch((err) => err);
     };
@@ -16,8 +17,20 @@ export default class MyRecipeService{
         return await axios.post(`recipe/details`, { id : _id, application_type : 'my_choice'}).then((res) => res.data).catch((err) => err);
     }
 
+    async getOptionDetails(op_type, op_val){
+        return await axios.post(`application/options_details`, { lang : 'KO', option_type: op_type, option_val : op_val }).then((res) => res.data).catch((err) => err);
+    }
+
     async getSingleRecipeProductDetails(_id, type){
         return await axios.post(`recipe/details`, { id : _id, application_type : type}).then((res) => res.data).catch((err) => err);
+    }
+
+    async editRecipeDetail(_id, _title, _additional_req){
+        return await axios.post(`recipe/details`, { id : _id, additional_request : _additional_req, title : _title}).then((res) => res.data).catch((err) => err);
+    }
+
+    async submitRecipeApplication(app_id){
+        return await axios.post(`recipe/details`, { application_id : app_id }).then((res) => res.data).catch((err) => err);
     }
 
     async deleteRecipeData(_id){
