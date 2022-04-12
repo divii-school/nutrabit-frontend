4537c7020ab38eaa82467390d0e18e7ae9bc1a00
AkshayaHan <98077602+AkshayaHan@users.noreply.github.com>
2022-04-12 12:45:45 +0530

    updated file

--- nutrabbit/src/pages/OnlyMe/MyApplicationDetails.vue
+++ nutrabbit/src/pages/OnlyMe/MyApplicationDetails.vue
@@ -34,32 +34,21 @@
                                 </thead>
                                 <tbody>
                                   <tr v-for="(item, index) of applicationList" :key="index">
-                                    <td>{{item.id}}</td>
-                                    <!-- <td @click="secondpage(item.id)">{{item.explanation}}</td> -->
+                                    <td>{{index +1}}</td>
                                     <td @click="this.$router.push(`/my-recipe-details-sample/${item.id}`)">{{item.explanation}}</td>
                                     <td>{{dateformat(item.createdDate)}}</td>
                                   </tr>
-                                  <!-- <tr>
-                                    <td>2</td>
-                                    <td>Raw material</td>
-                                    <td>aloe gel</td>
-                                  </tr>
-                                  <tr>
-                                    <td>3</td>
-                                    <td>Raw material</td>
-                                    <td>aloe gel</td>
-                                  </tr> -->
                                 </tbody>
                               </table>
                             </div>
                           </div>
                         </div>
-                        <pagination
+                        <!-- <pagination
                           v-model="page"
                           :records="5"
                           :per-page="1"
                           @paginate="myCallback"
-                        />
+                        /> -->
                       </div>
                       <div class="choice-selection-item-wrap choice-wrap-border">
                         <div class="choice-selection-item raw-material-product">
@@ -74,40 +63,65 @@
                                 <thead>
                                   <tr>
                                     <th>No</th>
-                                    <th>category</th>
                                     <th>Explanation</th>
+                                    <th>Application Date</th>
                                   </tr>
                                 </thead>
                                 <tbody>
-                                  <tr>
-                                    <td>One</td>
-                                    <td>Raw material</td>
-                                    <td>aloe gel</td>
-                                  </tr>
-                                  <tr>
-                                    <td>2</td>
-                                    <td>Raw material</td>
-                                    <td>aloe gel</td>
-                                  </tr>
-                                  <tr>
-                                    <td>3</td>
-                                    <td>Raw material</td>
-                                    <td>aloe gel</td>
+                                  <tr v-for="(item, index) of applicationList2" :key="index">
+                                    <td>{{index +1}}</td>
+                                    <td @click="this.$router.push(`/my-recipe-details-sample/${item.id}`)">{{item.explanation}}</td>
+                                    <td>{{dateformat(item.createdDate)}}</td>
                                   </tr>
                                 </tbody>
                               </table>
                             </div>
                           </div>
                         </div>
-                        <Pagination />
+                        <!-- <Pagination /> -->
                       </div>
-                  </div>
+                      <div class="choice-selection-item-wrap choice-wrap-border">
+                        <div class="choice-selection-item raw-material-product">
+                          <div class="heading-wrap">
+                            <div class="heading">
+                              <h2>My Choice</h2>
+                            </div>
+                          </div>
+                          <div class="materialForm">
+                            <div class="tableWrap">
+                              <table>
+                                <thead>
+                                  <tr>
+                                    <th>No</th>
+                                    <th>Explanation</th>
+                                    <th>Application Date</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr v-for="(item, index) of newApplicationList3" :key="index">
+                                    <td>{{index +1}}</td>
+                                    <td @click="this.$router.push(`/my-recipe-details-sample/${item.id}`)">{{item.explanation}}</td>
+                                    <td>{{dateformat(item.createdDate)}}</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                          </div>
+                        </div>
+                        <pagination
+                          v-model="page"
+                          :records="mychoice_total"
+                          :per-page="limit"
+                          @paginate="myCallback"
+                        />
+                      </div>
+                    </div>
                   <div v-show="currentTab === 1">
                     <div class="choice-selection-item-wrap choice-wrap-border">
                     <div class="choice-selection-item raw-material-product">
                       <div class="heading-wrap">
                         <div class="heading">
-                          <h2>Recommended blending</h2>
+                          <h2>nutri 3.3</h2>
                         </div>
                       </div>
                       <div class="materialForm">
@@ -116,38 +130,28 @@
                             <thead>
                               <tr>
                                 <th>No</th>
-                                <th>category</th>
                                 <th>Explanation</th>
+                                <th>Application Date</th>
                               </tr>
                             </thead>
                             <tbody>
-                              <tr>
-                                <td>One</td>
-                                <td>Raw material</td>
-                                <td>aloe gel</td>
-                              </tr>
-                              <tr>
-                                <td>2</td>
-                                <td>Raw material</td>
-                                <td>aloe gel</td>
-                              </tr>
-                              <tr>
-                                <td>3</td>
-                                <td>Raw material</td>
-                                <td>aloe gel</td>
+                              <tr v-for="(item, index) of quotationList" :key="index">
+                                <td>{{index +1}}</td>
+                                <td @click="$router.push({name : 'MyApplicationQuoteRequestBlending', params : { id : item.id ,  type : 'nutri'}})">{{item.explanation}}</td>
+                                <td>{{dateformat(item.createdDate)}}</td>
                               </tr>
                             </tbody>
                           </table>
                         </div>
                       </div>
                     </div>
-                    <Pagination />
+                    <!-- <Pagination /> -->
                   </div>
                     <div class="choice-selection-item-wrap choice-wrap-border">
                       <div class="choice-selection-item raw-material-product">
                         <div class="heading-wrap">
                           <div class="heading">
-                            <h2>nutri 3.3</h2>
+                            <h2>Recommended blending</h2>
                           </div>
                         </div>
                         <div class="materialForm">
@@ -156,32 +160,52 @@
                               <thead>
                                 <tr>
                                   <th>No</th>
-                                  <th>category</th>
                                   <th>Explanation</th>
+                                  <th>Application Date</th>
                                 </tr>
                               </thead>
                               <tbody>
-                                <tr>
-                                  <td>One</td>
-                                  <td>Raw material</td>
-                                  <td>aloe gel</td>
-                                </tr>
-                                <tr>
-                                  <td>2</td>
-                                  <td>Raw material</td>
-                                  <td>aloe gel</td>
-                                </tr>
-                                <tr>
-                                  <td>3</td>
-                                  <td>Raw material</td>
-                                  <td>aloe gel</td>
-                                </tr>
+                                <tr v-for="(item, index) of quotationList2" :key="index">
+                                  <td>{{index +1}}</td>
+                                  <td @click="$router.push({name : 'MyApplicationQuoteRequestBlending', params : { id : item.id ,  type : 'recommended'}})">{{item.explanation}}</td>
+                                  <td>{{dateformat(item.createdDate)}}</td>
+                              </tr>
                               </tbody>
                             </table>
                           </div>
                         </div>
                       </div>
-                      <Pagination />
+                      <!-- <Pagination /> -->
+                    </div>
+                    <div class="choice-selection-item-wrap choice-wrap-border">
+                      <div class="choice-selection-item raw-material-product">
+                        <div class="heading-wrap">
+                          <div class="heading">
+                            <h2>My Choice</h2>
+                          </div>
+                        </div>
+                        <div class="materialForm">
+                          <div class="tableWrap">
+                            <table>
+                              <thead>
+                                <tr>
+                                  <th>No</th>
+                                  <th>Explanation</th>
+                                  <th>Application Date</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr v-for="(item, index) of quotationList3" :key="index">
+                                  <td>{{index +1}}</td>
+                                  <td @click="$router.push({name : 'MyApplicationQuoteRequestChoice', params : { id : item.id }})">{{item.explanation}}</td>
+                                  <td>{{dateformat(item.createdDate)}}</td>
+                              </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- <Pagination /> -->
                     </div>
                   </div>
               </div>
@@ -215,16 +239,28 @@ export default {
         currentTab: 0,
         tabs: ['Sample Application Details', 'Quotation Request Details'],
         applicationList: [],
+        applicationList2: [],
+        applicationList3: [],
+        application_type:"nutri_blending", 
+        application_type2:"recommended_blending",
+        application_type3:"my_choice",
+        quotationList:[],
+        quotationList2:[],
+        quotationList3:[],
         // UpdatedapplicationList: [],
         // page: 1,
         // perPage: 5,
         userId: this.common.state.userId,
         lang:"KO",
-        application_type:"",
         page:1,
-        limit:10,
+        limit:5,
         sortBy: "",
         sortOrder: "",
+        nutri_blending_total:"",
+        mychoice_total:"",
+        recommended_total: "",
+
+        newApplicationList3: [],
       };
   },
 
@@ -239,12 +275,13 @@ export default {
 
   methods: {
 
-    // myCallback(ClickPage){
-    //   const startIndex = (ClickPage - 1) * this.perPage;
-    //   // const endIndex = (this.perPage * ClickPage);
-    //   const endIndex = startIndex + this.perPage;
-    //   this.UpdatedapplicationList = this.applicationList.slice(startIndex, endIndex);
-    // },
+    myCallback(ClickPage){
+      const startIndex = (ClickPage - 1) * this.limit;
+      // const endIndex = (this.perPage * ClickPage);
+      const endIndex = startIndex + this.limit;
+      this.newApplicationList3 = this.applicationList3.slice(startIndex, endIndex);
+      console.log("this.newApplicationList3",this.newApplicationList3);
+    },
 
     applicationlist() {
       this.myApplicationDetails.applicationlist(
@@ -260,7 +297,125 @@ export default {
         if (res.status == 200) {
           console.log("res",res);
           this.applicationList = res.data.data.applicationData;
-          console.log("this.applicationList",this.applicationList);
+          this.nutri_blending_total = res.data.data.total;
+          console.log("nutri_blending",this.nutri_blending_total);
+          // this.myCallback(1);
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    },
+
+    applicationlist2() {
+      this.myApplicationDetails.applicationlist2(
+        this.userId,
+        this.lang,
+        this.application_type2,
+        this.page,
+        this.limit,
+        this.sortBy,
+        this.sortOrder,
+        )
+        .then((res) => {
+        if (res.status == 200) {
+          console.log("res",res);
+          this.applicationList2 = res.data.data.applicationData;
+          this.recommended_total = res.data.data.total;
+          console.log("MyChoice",this.recommended_total);
+          // this.myCallback(1);
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    },
+
+    applicationlist3() {
+      this.myApplicationDetails.applicationlist3(
+        this.userId,
+        this.lang,
+        this.application_type3,
+        this.page,
+        this.limit,
+        this.sortBy,
+        this.sortOrder,
+        )
+        .then((res) => {
+        if (res.status == 200) {
+          console.log("res",res);
+          this.applicationList3 = res.data.data.applicationData;
+          this.mychoice_total = res.data.data.total;
+          console.log("MyChoice",this.mychoice_total);
+          this.myCallback(1);
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    },
+
+    quotationlist() {
+      this.myApplicationDetails.quotationlist(
+        this.userId,
+        this.lang,
+        this.application_type,
+        this.page,
+        this.limit,
+        this.sortBy,
+        this.sortOrder,
+        )
+        .then((res) => {
+        if (res.status == 200) {
+          console.log("res",res);
+          this.quotationList = res.data.data.applicationData;
+          console.log("quotation_nutri_blending",this.quotationList);
+          // this.myCallback(1);
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    },
+
+    quotationlist2() {
+      this.myApplicationDetails.quotationlist2(
+        this.userId,
+        this.lang,
+        this.application_type2,
+        this.page,
+        this.limit,
+        this.sortBy,
+        this.sortOrder,
+        )
+        .then((res) => {
+        if (res.status == 200) {
+          console.log("res",res);
+          this.quotationList2 = res.data.data.applicationData;
+          console.log("quotationRecommended",this.quotationList2);
+          // this.myCallback(1);
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    },
+
+    quotationlist3() {
+      this.myApplicationDetails.quotationlist3(
+        this.userId,
+        this.lang,
+        this.application_type3,
+        this.page,
+        this.limit,
+        this.sortBy,
+        this.sortOrder,
+        )
+        .then((res) => {
+        if (res.status == 200) {
+          console.log("res",res);
+          this.quotationList3 = res.data.data.applicationData;
+          console.log("quotationMyChoice",this.quotationList3);
           // this.myCallback(1);
         }
       })
@@ -275,15 +430,17 @@ export default {
       }
     },
 
-    // secondpage(id){
-    //   window.location = "/my-recipe-details-sample" + id;
-    // },
-
   },
 
   mounted() {
     this.applicationlist();
     this.dateformat();
+    this.quotationlist();
+    this.quotationlist2();
+    this.quotationlist3();
+
+    this.applicationlist2();
+    this.applicationlist3();
   }
   
 };
--- nutrabbit/src/services/MyApplicationDetails.js
+++ nutrabbit/src/services/MyApplicationDetails.js
@@ -20,4 +20,84 @@ export default class MyApplicationDetails {
           sortOrder:sortOrder,
         }).then((res) => res).catch((err) => err);
     }
+
+    async applicationlist2(userId, lang, application_type2, page, limit, sortBy, sortOrder) {
+      return await axios.post(`/application/sample_application`,
+        {
+          userId: userId,
+          lang: lang,
+          application_type:application_type2,
+          page:page,
+          limit: limit,
+          sortBy: sortBy,
+          sortOrder:sortOrder,
+        }).then((res) => res).catch((err) => err);
+    }
+
+    async applicationlist3(userId, lang, application_type3, page, limit, sortBy, sortOrder) {
+      return await axios.post(`/application/sample_application`,
+        {
+          userId: userId,
+          lang: lang,
+          application_type:application_type3,
+          page:page,
+          limit: limit,
+          sortBy: sortBy,
+          sortOrder:sortOrder,
+        }).then((res) => res).catch((err) => err);
+    }
+
+    async quotationlist(userId, lang, application_type, page, limit, sortBy, sortOrder) {
+      return await axios.post(`/application/get_quote`,
+        {
+          userId: userId,
+          lang: lang,
+          application_type:application_type,
+          page:page,
+          limit: limit,
+          sortBy: sortBy,
+          sortOrder:sortOrder,
+        }).then((res) => res).catch((err) => err);
+    }
+
+    async quotationlist2(userId, lang, application_type2, page, limit, sortBy, sortOrder) {
+      return await axios.post(`/application/get_quote`,
+        {
+          userId: userId,
+          lang: lang,
+          application_type:application_type2,
+          page:page,
+          limit: limit,
+          sortBy: sortBy,
+          sortOrder:sortOrder,
+        }).then((res) => res).catch((err) => err);
+    }
+
+    async quotationlist3(userId, lang, application_type3, page, limit, sortBy, sortOrder) {
+      return await axios.post(`/application/get_quote`,
+        {
+          userId: userId,
+          lang: lang,
+          application_type:application_type3,
+          page:page,
+          limit: limit,
+          sortBy: sortBy,
+          sortOrder:sortOrder,
+        }).then((res) => res).catch((err) => err);
+    }
+
+    async sampledetail(id) {
+      return await axios.post(`/application/my_choice/details`,
+        {
+          id:id,
+        }).then((res) => res).catch((err) => err);
+    }
+
+    async getsampleDetails(op_type,op_val) {
+      return await axios.post(`/application/options_details`,
+        {
+          option_type:op_type,
+          option_val:op_val,
+        }).then((res) => res).catch((err) => err);
+    }
 }
