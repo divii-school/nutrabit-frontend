41de30e6198f94849e7beb25ed231d573b2e88e1
aniruddhadivii <aniruddha@divii.com>
2022-04-14 15:12:45 +0530

    up

---
+++ nutrabbit/src/components/CacaoChat.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="totop-wrap">
+    <span class="round">
+      <img src="../assets/images/chat-icon.svg" />
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "CacaoChat",
+};
+</script>
