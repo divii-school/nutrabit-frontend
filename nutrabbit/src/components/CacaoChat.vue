855b3699bfbe113a51f245e865af8cc7ecc9e59b
aniruddhadivii <aniruddha@divii.com>
2022-04-22 11:52:38 +0530

    up

--- nutrabbit/src/components/CacaoChat.vue
+++ nutrabbit/src/components/CacaoChat.vue
@@ -11,20 +11,9 @@ export default {
   methods: {
     chatWithKakao() {
       window.Kakao.init("5d14c5e0ea3ead3c0683355cba9eda57");
-      Kakao.Channel.createAddChannelButton({
-        container: ".kakao-talk-chat",
+      Kakao.Channel.chat({
         channelPublicId: "_qaBKxb",
-        size: "small",
-        supportMultipleDensities: true,
       });
-      (function (d, s, id) {
-        let js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://developers.kakao.com/sdk/js/kakao.channel.min.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      })(document, "script", "kakao-js-sdk");
     },
   },
 };
