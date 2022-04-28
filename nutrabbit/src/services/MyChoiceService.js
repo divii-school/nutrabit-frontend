35a0cd1265ca7b41a52304510e66e5b9c425c14f
kakalidivii <kakali@divii.com>
2022-04-28 18:25:20 +0530

    up

--- nutrabbit/src/services/MyChoiceService.js
+++ nutrabbit/src/services/MyChoiceService.js
@@ -58,7 +58,7 @@ export default class MyChoiceService {
 
   //recommended blending package add API Functions
   async getRecommendedBlendingPackageAdd(blending_id, package_id, etc, additional_request, service_type, is_temporary_storage) {
-    return await axios.post(`/blending/recommended/add`, { blending_id: blending_id, package_id: package_id, etc:etc, additional_request: additional_request, service_type: service_type, is_temporary_storage: is_temporary_storage }).then((res) => res.data).catch((err) => err);
+    return await axios.post(`/blending/recommended/add`, { blending_id: blending_id, package_id: package_id, etc:etc, additional_request: additional_request, service_type: service_type, is_temporary_storage: is_temporary_storage, lang: 'KO' }).then((res) => res.data).catch((err) => err);
   }
 
   //blending formulation API Functions
@@ -88,7 +88,7 @@ export default class MyChoiceService {
 
   //raw material package add API Functions
   async getRawMaterialPackageAdd(raw_material_ids, pill_id, package_id, title, additional_request, service_type, is_temporary_storage) {
-    return await axios.post(`/blending/raw_material/add`, { raw_material_ids: raw_material_ids, pill_id: pill_id, package_id: package_id, title: title, additional_request: additional_request, service_type: service_type, is_temporary_storage: is_temporary_storage }).then((res) => res.data).catch((err) => err);
+    return await axios.post(`/blending/raw_material/add`, { raw_material_ids: raw_material_ids, pill_id: pill_id, package_id: package_id, title: title, additional_request: additional_request, service_type: service_type, is_temporary_storage: is_temporary_storage, lang: 'KO' }).then((res) => res.data).catch((err) => err);
   }
 
   //raw material storage box API Functions
