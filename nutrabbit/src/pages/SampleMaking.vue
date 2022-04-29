73778b35ba72f29afdcc6d06cb7c724001bfb75b
AkshayaHan <98077602+AkshayaHan@users.noreply.github.com>
2022-04-29 13:40:21 +0530

    updated file

--- nutrabbit/src/pages/SampleMaking.vue
+++ nutrabbit/src/pages/SampleMaking.vue
@@ -38,18 +38,20 @@
             <p class="title text-center samllTitle">my choice</p>
             <h4 class="title text-center">{{ $t("sampleMaking.sampleFaq.title") }}</h4>
             <div id="app" class="accCont">
-              <accordion class="mb-4">
+              <accordion class="mb-4" v-for="(item, index) of faqList" :key="index">
                 <template v-slot:title>
-                  <span class="font-semibold text-xl accTItle">{{ $t("sampleMaking.sampleFaq.ques1") }}</span>
+                  <!-- <span class="font-semibold text-xl accTItle">{{ $t("sampleMaking.sampleFaq.ques1") }}</span> -->
+                  <span class="font-semibold text-xl accTItle">{{ item.title_en }}</span>
                 </template>
                 <template v-slot:content>
                   <p class="accBody">
-                    {{ $t("sampleMaking.sampleFaq.ans1") }}
+                    <!-- {{ $t("sampleMaking.sampleFaq.ans1") }} -->
+                     {{ item.description_en }}
                   </p>
                 </template>
               </accordion>
 
-              <accordion class="mb-4">
+              <!-- <accordion class="mb-4">
                 <template v-slot:title>
                   <span class="font-semibold text-xl accTItle">{{ $t("sampleMaking.sampleFaq.ques2") }}</span>
                 </template>
@@ -58,7 +60,7 @@
                     {{ $t("sampleMaking.sampleFaq.ans2") }}
                   </p>
                 </template>
-              </accordion>
+              </accordion> -->
             </div>
           </div>
         </div>
@@ -69,6 +71,7 @@
 
 <script>
 import Accordion from "../components/Accordion.vue";
+import CustomerCenterService from "../services/CustomerCenterService";
 export default {
   
   name: "Samplemaking",
@@ -77,7 +80,10 @@ export default {
   },
   data() {
     return {
-      
+      faqList: [],
+      totalfaqlist:[],
+      // page: 1,
+      // limit: 4,
     };
   },
 
@@ -85,36 +91,62 @@ export default {
     processData() { return[
         {
           img: "../../../src/assets/images/step1.png",
-          step: "Step 1",
+          step: "STEP.1",
           title: this.$t("sampleMaking.sampleprocress.title1"),
           desc: this.$t("sampleMaking.sampleprocress.desc1")
         },
         {
           img: "../../../src/assets/images/step2.png",
-          step: "Step 2",
+          step: "STEP.2",
           title: this.$t("sampleMaking.sampleprocress.title2"),
           desc: this.$t("sampleMaking.sampleprocress.desc2")
         },
         {
           img: "../../../src/assets/images/step3.png",
-          step: "Step 3",
+          step: "STEP.3",
           title: this.$t("sampleMaking.sampleprocress.title3"),
           desc: this.$t("sampleMaking.sampleprocress.desc3")
         },
         {
           img: "../../../src/assets/images/step4.png",
-          step: "Step 4",
+          step: "STEP.4",
           title: this.$t("sampleMaking.sampleprocress.title4"),
           desc: this.$t("sampleMaking.sampleprocress.desc4")
         },
         {
           img: "../../../src/assets/images/step5.png",
-          step: "Step 5",
+          step: "STEP.5",
           title: this.$t("sampleMaking.sampleprocress.title5"),
           desc: this.$t("sampleMaking.sampleprocress.desc5")
         },
       ]
     }
+  },
+
+  created() {
+    this.CustomerCenterService = new CustomerCenterService();
+  },
+
+  methods: {
+    allFaqList() {
+      this.CustomerCenterService.getFAQList()
+
+        .then((res) => {
+          if (res.status == 200) {
+            this.totalfaqlist = res.data.data.faq;
+
+            const size = 4
+            this.faqList = this.totalfaqlist.slice(0, size);
+          }
+        })
+        .catch((err) => {
+         return;
+        });
+    },
+  },
+
+  mounted() {
+    this.allFaqList();
   },
 };
 </script>
