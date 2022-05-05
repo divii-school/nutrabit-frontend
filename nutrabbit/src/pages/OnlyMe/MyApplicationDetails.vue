0f259597d8c468a9b9c78a60e9e63075269fe07b
AkshayaHan <98077602+AkshayaHan@users.noreply.github.com>
2022-05-05 17:28:11 +0530

    updated file

--- nutrabbit/src/pages/OnlyMe/MyApplicationDetails.vue
+++ nutrabbit/src/pages/OnlyMe/MyApplicationDetails.vue
@@ -400,7 +400,7 @@ export default {
   },
   updated(){
     this.globalLocale = localStorage.getItem('selectedLang');
-    console.log(this.globalLocale)
+    // console.log(this.globalLocale)
   },
 
   watch: {
