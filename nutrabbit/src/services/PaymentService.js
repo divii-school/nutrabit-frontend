dc24e5295c0cc0e0e300db1020fb518b0b0536a7
Bikramjit Show <bikramjit@divii.com>
2022-05-11 13:57:44 +0530

    add

--- nutrabbit/src/services/PaymentService.js
+++ nutrabbit/src/services/PaymentService.js
@@ -1,3 +1,4 @@
+import state from '../store/common';
 var IMP = window.IMP;
 // IMP.init("imp75847396");
 IMP.init("imp55488636");
@@ -5,43 +6,37 @@ IMP.init("imp55488636");
 // const IMP = window.IMP; 
 // IMP.init("{INIpayTest}"); 
 export default class PaymentService {
-  requestPay() {
+  requestPay(buyerEmail, buyerName, buyerTel, buyerAddr) {
     // IMP.request_pay(param, callback) call payment window
     IMP.request_pay({
       //pg: "html5_inicis",
       pg: "uplus",
-      pay_method: "card",
+      // pay_method: "card",
       merchant_uid: "ORDER_" + new Date().getTime(),
-      name: "Norway swivel chair",
-      amount: 64000,
-      buyer_email: "johndoe@gmail.com",
-      buyer_name: "John Doe",
-      buyer_tel: "010-4242-4242",
-      buyer_addr: "Shinsa-dong, Gangnam-gu, Seoul",
-      buyer_postcode: "01181",
+      // name: "Norway swivel chair",
+      amount: 300000,
+      buyer_email: buyerEmail,
+      buyer_name: buyerName,
+      buyer_tel: buyerTel,
+      buyer_addr: buyerAddr,
       app_scheme: "NutrabbitIAmPort",
       //m_redirect_url :'{URL to redirect to after payment approval on Mobile}'
     }, function (rsp) {
       if (rsp.success) { // payment successful: payment accepted or virtual account issued
         alert('"Payment Success. Success:' + rsp);
         console.log(rsp);
-        /*
-        // jQuery HTTP request
-        jQuery.ajax({
-            url: "{server-side endpoint to process the payment information}", // Example: https://www.myservice.com/payments/complete
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            data: {
-                imp_uid: rsp.imp_uid,
-                merchant_uid: rsp.merchant_uid
-            }
-        }).done(function (data) {
-          // Logic for when the server successfully processes the payment information
-        })
-        */
+        state.isPayment = true;
+        state.isPaymentDone = true;
       } else {
         console.log(rsp);
+        state.isPayment = false;
+        state.isPaymentDone = true;
+        // addPayment(applicationId, applyNum, bankName, buyerAddr, buyerEmail, buyerName, buyerTel, 
+        //   cardName, cardNumber, cardQuota, currency, customData, impUid, merchantUid, name, 
+        //   paidAmount, paidAt, payMethod, pgProvider, pgTid, pgType, receiptUrl, requestId, status, 
+        //   success, errorCode, errorMsg);
         alert("Payment failed. Error: " + rsp.error_msg);
+
       }
 
     });
