88bd53a5eb04eeee6438c1178e04cf166c2abac0
Bikramjit Show <bikramjit@divii.com>
2022-06-14 15:35:15 +0530

    add

--- nutrabbit/src/config/axios-interceptor.js
+++ nutrabbit/src/config/axios-interceptor.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 import Toast from '../alert/alert.js';
 
 window.axios = axios;
-axios.defaults.baseURL = 'https://api-nutrabbit-dev.dvconsulting.org/nutrabbit-dev/api/v1/sites/';
+// axios.defaults.baseURL = 'https://api-nutrabbit-dev.dvconsulting.org/nutrabbit-dev/api/v1/sites/'; // local
+// axios.defaults.baseURL = 'https://back.nutri33.co.kr/'; // live
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL; // live
 
 axios.interceptors.request.use(function (config) {
     const token = localStorage.getItem('token');
