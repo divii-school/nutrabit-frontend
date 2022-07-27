d7adaaa7f91263b625c50379ca8ca9b525dabf46
Phenil810 <100286762+Phenil810@users.noreply.github.com>
2022-07-27 17:07:57 +0530

    updated

--- nutrabit-admin/src/pages/BannerManagemet/ViewBanner.vue
+++ nutrabit-admin/src/pages/BannerManagemet/ViewBanner.vue
@@ -30,7 +30,7 @@
                             alt="이미지를 사용할 수 없음"
                             class="product-image"
                         />
-                        <!-- <img
+                         <!-- <img 
                             :src="'https://back.nutri33.co.kr/public/' + mydata.desktop_banner"
                             alt="이미지를 사용할 수 없음"
                             class="product-image"
